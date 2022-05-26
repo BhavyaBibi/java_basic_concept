ed74a98b49c068c4dffc2ef386d366000968bccf
BhavyaBibi <bhavyabibi007@gmail.com>
2022-05-26 17:52:37 +0400

    oneof the javascript coe is added

---
+++ basicprogram.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        
+        System.out.println("enter a number:" );
+        int a = s.nextInt();
+        int b = s.nextInt();
+        
+        System.out.println( a +" " + b);
+        
+    }
+}
