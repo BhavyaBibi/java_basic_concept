d063860cb5b1e09562b3ace32635977ddbc1d353
BhavyaBibi <bhavyabibi007@gmail.com>
2022-05-26 17:56:23 +0400

    use to need to know the  2 numbers

--- basicprogram.java
+++ basicprogram.java
@@ -4,7 +4,7 @@ public class Main
     public static void main(String[] args) {
         Scanner s =new Scanner(System.in);
         
-        System.out.println("enter a number:" );
+        System.out.println("enter two number:" );
         int a = s.nextInt();
         int b = s.nextInt();
         
