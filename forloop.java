9a9945fcbdfec965c20c965e8ab5ba5f18ae62e6
BhavyaBibi <bhavyabibi007@gmail.com>
2022-05-31 09:54:28 +0400

    this woking in  the befoe for is in the loop

---
+++ forloop.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+        
+        System.out.println("enter a number:" );
+        int num = sc.nextInt();//int:nextint()
+        int sum =0;
+        
+        for(int i=0; i<num; i++){
+            sum += i;
+        }
+        System.out.println("the result is "+ sum);
+
+            }
+}
+
+#output
+
